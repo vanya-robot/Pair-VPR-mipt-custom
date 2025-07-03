3b6b4c513586c23734c09d964fe5bf6630a0f317
vanya-robot <alexeenkoyaroslav@mail.ru>
2025-07-03 15:39:34 +0300

    kk

--- itlp_eval.py
+++ itlp_eval.py
@@ -199,8 +199,12 @@ class ITLPEvaluator:
             best_score1 = -float('inf')
             best_match1 = 0
             for db_idx in top1_indices[0]:
+                print(db_dense_features[db_idx].shape)
                 db_dense = db_dense_features[db_idx].unsqueeze(0).to(self.device)
+                print('db after:', db_dense.shape)
+                print(dense2.shape)
                 current_dense = dense1.unsqueeze(0).to(self.device)
+                print('cur after:', current_dense.shape)
                 
                 score1 = self.model(current_dense, db_dense, mode="pairvpr")
                 score2 = self.model(db_dense, current_dense, mode="pairvpr")
@@ -214,12 +218,11 @@ class ITLPEvaluator:
             best_score2 = -float('inf')
             best_match2 = 0
             for db_idx in top2_indices[0]:
-                print(db_dense_features[db_idx].shape())
+                
                 db_dense = db_dense_features[db_idx].unsqueeze(0).to(self.device)
-                print('db after:', db_dense.shape())
-                print(dense2.shape())
+
                 current_dense = dense2.unsqueeze(0).to(self.device)
-                print('cur after:', current_dense.shape())
+
                 
                 score1 = self.model(current_dense, db_dense, mode="pairvpr")
                 score2 = self.model(db_dense, current_dense, mode="pairvpr")
